501d25823ff3a4e987ddf6d7cf71e9a30258c79f
Cameron S. Williamson <therealkey@outlook.com>
2021-11-16 14:26:16 -0800

    Added a user profile page and a card component that holds post information

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import Content from "../components/Content";
+import PostCard from "../components/PostCard";
 
 const UserProfile = () => {
     const { username } = useParams();
 
-    return <div>hi there {username}</div>;
+    return (
+        <Content>
+            <h1>{username}</h1>
+            <PostCard title="first post" content="" postID={100000} />
+        </Content>
+    );
 };
 
 export default UserProfile;
