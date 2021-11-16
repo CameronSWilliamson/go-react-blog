c15c6b11055134b145f663c309ad7597f77d95ec
Cameron S. Williamson <therealkey@outlook.com>
2021-11-15 18:29:26 -0800

    Added a HomePage and a UserProfile page

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Content from "../components/Content";
+
+const HomePage = () => {
+    return (
+        <Content>
+            <h1>Blog Site!</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+                consequatur facere sapiente. Quaerat dolore, aliquid totam
+                aperiam corrupti maiores commodi impedit quisquam recusandae id
+                adipisci, et voluptas explicabo ducimus. Saepe? Lorem ipsum
+                dolor sit, amet consectetur adipisicing elit. Quis, eaque
+                repellat voluptas ab distinctio eum provident! Aspernatur dolor,
+                vel voluptatem tenetur nulla dignissimos unde id consectetur
+                iusto quisquam veritatis impedit? Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Error quae reprehenderit,
+                repudiandae officia sint ducimus iusto assumenda doloremque
+                tempore autem aut atque soluta quidem porro asperiores ad ut
+                molestiae! Dolore? Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Reprehenderit, labore aut? Quis, quos nulla et
+                eum quam porro quo. Est nisi eveniet, atque aut illum dolor
+                natus. Dolorem, fugiat nostrum.
+            </p>
+        </Content>
+    );
+};
+
+export default HomePage;
