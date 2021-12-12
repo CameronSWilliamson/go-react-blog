56131c9fc5f49514d4a9a9e8d43da259ff27de5d
Cameron S. Williamson <therealkey@outlook.com>
2021-12-12 01:09:58 +0000

    Added log out button to the user profile page if they are logged in

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Content from "../components/Content";
 import PostCard from "../components/PostCard";
+import UserPostList from "../components/UserPostList";
+import { logOut } from "../Utils/Auth";
+import { UsernameProp } from "../Utils/Types/Props";
 
-const UserProfile = () => {
+const UserProfile = (props: UsernameProp) => {
     const { username } = useParams();
+
+    const loggingOut = () => {
+        logOut();
+        props.setLoggedIn(false);
+        props.setUsername("");
+    };
 
     return (
         <Content>
             <h1>{username}</h1>
             <PostCard title="first post" content="" postID={100000} />
+            <UserPostList username={username!} />
+            {props.loggedIn && props.username === username ? (
+                <Button onClick={loggingOut}>logOut</Button>
+            ) : (
+                <p></p>
+            )}
         </Content>
     );
 };
