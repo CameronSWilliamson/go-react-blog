f53a3debe08c851d21d4044ce489c4e2ecc8c699
Cameron S. Williamson <therealkey@outlook.com>
2021-12-16 19:07:26 +0000

    Improved User List for UserSearch

--- client/src/pages/UserSearch.tsx
+++ client/src/pages/UserSearch.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import Content from "../components/Content";
+import { User } from "../Utils/Types/Models";
 
 const UserSearch = () => {
+    const [users, setUsers] = React.useState<Array<User>>([]);
+
     useEffect(() => {
         fetch("/api/users", {
             method: "GET",
@@ -9,18 +12,26 @@ const UserSearch = () => {
                 "Content-Type": "application/json",
             },
         }).then((response) => {
-            response.json().then((data) => {
-                console.log(data);
+            response.text().then((res) => {
+                let response: Array<User> = JSON.parse(res);
+                console.log(response);
+                setUsers(response);
             });
         });
-    });
+    }, []);
 
     return (
         <Content>
             <ul>
-                <li>
-                    <a href="/users/therealkey">Therealkey</a>
-                </li>
+                {users.map((user) => {
+                    return (
+                        <li>
+                            <a href={"../users/" + user.username}>
+                                {user.username}
+                            </a>
+                        </li>
+                    );
+                })}
             </ul>
         </Content>
     );
